6da8a52288d22cf4fbbc75b78b359cd1b5857878
ivan-602 <99512638+ivan-602@users.noreply.github.com>
2022-10-23 17:11:00 +0200

    edit

--- js/first-part.js
+++ js/first-part.js
@@ -1,11 +1,44 @@
 const refs = {
+  preLoad: document.querySelector(".preload-section"),
   preLoading: document.querySelector(".pre-loading"),
   sphereContainer: document.querySelector(".sphere-container"),
+  kostyaSection: document.querySelector(".kostya-section"),
+  blowingSymbols: document.querySelector(".blowing-symbols"),
+  confetti: document.querySelector(".confetti"),
+  stars: document.querySelector(".stars"),
+  bdHat: document.querySelector(".bd-hat"),
+  blueOvalLeft: document.querySelector(".blue-oval-left"),
+  blueCircle: document.querySelector(".blue-circle"),
+  blueOvalRight: document.querySelector(".blue-oval-right"),
 };
-removeElement(refs.preLoading, "pre-loading", 2200);
 
+refs.kostyaSection.classList.add("removed");
+refs.confetti.classList.add("removed");
+refs.stars.classList.add("removed");
+refs.bdHat.classList.add("removed");
+refs.blueOvalLeft.classList.add("removed");
+refs.blueCircle.classList.add("removed");
+refs.blueOvalRight.classList.add("removed");
+
+
+
+// * 1-st screen
+removeElement(refs.preLoading, "pre-loading", 2200);
+// * 2-nd screen
 sphere();
 removeElement(refs.sphereContainer, "sphere-container", 3900);
+removeElement(refs.preLoad, "preload-section", 3900);
+// * 3-d screen
+displayElement(refs.kostyaSection, 3900);
+removeElement(refs.blowingSymbols, "blowing-symbols", 7300);
+// * 4-th screen
+displayElement(refs.confetti, 7500);
+displayElement(refs.stars, 7300);
+displayElement(refs.bdHat, 7300);
+displayElement(refs.blueOvalLeft, 7300);
+displayElement(refs.blueCircle, 7300);
+displayElement(refs.blueOvalRight, 7300);
+
 
 function removeElement(element, initialClass, timeOut) {
   return setTimeout(changeClass, timeOut);
@@ -18,5 +51,12 @@ function sphere() {
   return setTimeout(sphereCreation, 2000);
   function sphereCreation() {
     refs.sphereContainer.firstElementChild.classList.add("sphere");
+  }
+}
+function displayElement(element, timeOut) {
+  return setTimeout(removeClass, timeOut);
+
+  function removeClass() {
+    return element.classList.remove("removed");
   }
 }
